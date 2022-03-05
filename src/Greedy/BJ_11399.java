1ce5e55e2c4b0c7d7818607bcf32e2bf9988138c
brenda <brenda524@naver.com>
2022-03-05 23:40:19 +0900

    Add better solution & Lack of understanding 'counting sort'

--- src/Greedy/BJ_11399.java
+++ src/Greedy/BJ_11399.java
@@ -1,10 +1,11 @@
 package Greedy;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+// 방법 1. ArrayList 사용
 public class BJ_11399 {
     public static void main(String[] args) {
         /**
@@ -18,7 +19,9 @@ public class BJ_11399 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();    //사람 수
         List<Integer> timeList = new ArrayList<>(); //걸리는 시간
-        timeList.add(sc.nextInt());
+        for(int i=0;i<N;i++){
+            timeList.add(sc.nextInt());             //
+        }
 
         /**
          * 문제 풀이 아이디어
@@ -32,8 +35,99 @@ public class BJ_11399 {
         for(int i=0;i<N;i++){
 //            min += timeList.get(i) * (N-i);
             wait_time += timeList.get(i);       //ERROR: Exception in thread "main" java.lang.IndexOutOfBoundsException: Index 1 out of bounds for length 1
+                                                //문제: Line20 코드를 N만큼 반복 안 돌렸음
             total_time += wait_time;
         }
         System.out.println(total_time);
     }
+}
+
+// 방법 2. Array 사용
+public class BJ_11399 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();    //사람 수
+        int[] timeArray = new int[N];
+        for(int i=0;i<N;i++){
+            timeArray[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(timeArray); //배열 정렬하는 법
+
+        int min = 0;
+        for(int i=0;i<N;i++){
+            min += timeArray[i] * (N-i);
+        }
+        System.out.println(min);
+    }
+}
+
+// 아래부터는 검색한 풀이
+// 방법 3. Scanner + Counting Sort
+public class BJ_11399 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        // 입력의 범위는 1 ~ 1000이므로 1001개의 index를 둔다.
+        int[] arr = new int[1001];
+
+        // Counting Sort
+        while (N-- > 0) {
+            arr[in.nextInt()]++;
+        }
+
+        int prev = 0;	// 이전까지의 대기시간 누적합
+        int sum = 0;	// 사람별 대기시간 총합
+
+        for (int i = 0; i < 1001; i++) {
+
+            // 해당 i index가 0이 될 때 까지 반복     // Q. 이 부분부터 이해가 안 돼!
+            while (arr[i]-- > 0) {
+                // 이전까지의 대기시간과 현재 사람이 걸리는 시간을 더해준다.
+                sum += (i + prev);
+
+                // 이전까지의 누적합에 현재 걸리는 시간을 더해준다.
+                prev += i;
+            }
+        }
+        System.out.println(sum);
+    }
+
+}
+
+// 방법 4. BufferedReader + Counting Sort
+public class BJ_11399 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        // 입력의 범위는 1 ~ 1000이므로 1001개의 index를 둔다.
+        int[] arr = new int[1001];
+
+        // Counting Sort
+        while (N-- > 0) {
+            arr[Integer.parseInt(st.nextToken())]++;
+        }
+
+        int prev = 0;	// 이전까지의 대기시간 누적합
+        int sum = 0;	// 사람별 대기시간 총합
+
+        for (int i = 0; i < 1001; i++) {
+
+            // 해당 i index가 0이 될 때 까지 반복     // Q. 이 부분부터 이해가 안 돼!
+            while (arr[i]-- > 0) {
+                // 이전까지의 대기시간과 현재 사람이 걸리는 시간을 더해준다.
+                sum += (i + prev);
+
+                // 이전까지의 누적합에 현재 걸리는 시간을 더해준다.
+                prev += i;
+            }
+        }
+        System.out.println(sum);
+    }
+
 }
