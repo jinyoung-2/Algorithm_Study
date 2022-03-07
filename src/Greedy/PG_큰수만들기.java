c28a058bde6991332566d0fbd6a374863b1fcc37
brenda <brenda524@naver.com>
2022-03-07 21:21:58 +0900

    다른 풀이 참고해서 idea 변경

--- src/Greedy/PG_큰수만들기.java
+++ src/Greedy/PG_큰수만들기.java
@@ -1,42 +1,35 @@
 package Greedy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class PG_큰수만들기 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /**
+         * String인 number에서 K개의 수 제거
+         * = number.length() - K 개의  수 선택
+         *
+         * ex) 23563850135에서 4개의 수 제거 = 12-4인 8개의 수 선택
+         * */
+
         Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+        int k = sc.nextInt();
+        StringBuilder answer = new StringBuilder();
 
-        String number = br.readLine();
-        int K = sc.nextInt();   //제거할 수의 개수
-
-        List<Integer> numCopy = new LinkedList<>();
-        List<Integer> result = new LinkedList<>();
-        StringTokenizer st = new StringTokenizer(number,"");
-        for(int i=0;i<number.length();i++){
-            numCopy.add(Integer.parseInt(st.nextToken()));
-            result.add(Integer.parseInt(st.nextToken()));
-        }
-
-        Collections.sort(result);
-
-        for(int i=0;i<number.length()-K;i++){
-            result.remove(i);
-        }
-
-        for(int i=0;i<numCopy.size();i++){
-            for(int j=0;j<result.size();j++){
-                if(numCopy.get(i) == result.get(j)){
-                    numCopy.remove(i);
-                    i--;
+        int idx = 0;    //선택된 문자의 index값
+        int comp = 0;   //선택된 문자의 숫자값
+        for(int i=number.length()-k;i>0;i--){   //남은 선택할 문자의 개수
+            comp = number.charAt(idx)-'0';  //선택할 문자 초기화
+            for(int j=idx;j<=number.length()-i;j++){   //최댓값 찾는 루프 => 여기 이해 안 돼.
+                if(comp < number.charAt(j)-'0'){
+                    comp = number.charAt(j)-'0';
+                    idx = j+1;
                 }
             }
+            answer.append(comp);
         }
-
-
-        System.out.println(numCopy.toString());
+        System.out.println(answer.toString());
     }
 }
