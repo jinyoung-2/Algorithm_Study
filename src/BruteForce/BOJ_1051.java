fa05db90172399fe26fb52cdfb10ca8145eac793
brenda <brenda524@naver.com>
2022-03-19 15:17:06 +0900

    코드 수정 및 런타임에러 발생

--- src/BruteForce/BOJ_1051.java
+++ src/BruteForce/BOJ_1051.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class BOJ_1051 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = sc.nextInt();   //가로
+        int M = sc.nextInt();   //세로
         int[] arr[] = new int[M][N];
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
@@ -17,18 +17,16 @@ public class BOJ_1051 {
         }
 
         // 꼭짓점에 쓰여 있는 수가 모두 같은 가장 큰 정사각형 찾기
-        int maxSize = 1;
+        int maxSize = 1;    // 0으로 두면 안 됨. 1*1은 가장 작은 정사각형이므로, 최소 정사각형 크기가 1 이상임
         int a = 2;
         int b = Math.min(N,M);
-        for (int i=a;i<=b;i++){
-            for(int j=0;j<M-i;j++){
-                for(int k=0;k<N-i;k++){
-                    if(arr[j][k]==arr[j][k+i]){
-                        if(arr[j][k]==arr[j+i][k]){
-                            if(arr[j][k]==arr[j+i][k+i]){
-//                                maxSize = arr[j][k];
-                                if(maxSize<i)
-                                   maxSize = i;
+        for (int i=a;i<=b;i++){ //정사각형 한 변의 길이의 범위 => 2<=i<=Math.min(N,M)
+            for(int j=0;j<=M-i;j++){ //세로
+                for(int k=0;k<=N-i;k++){ //가로
+                    if(arr[j][k]==arr[j][k+i-1]){
+                        if(arr[j][k]==arr[j+i-1][k]){
+                            if(arr[j][k]==arr[j+i-1][k+i-1]){
+                                maxSize = i;
                             }
                         }
 
