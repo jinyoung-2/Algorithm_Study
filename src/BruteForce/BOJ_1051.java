b59fdb6971c8775134c34300ef372497b6d071c5
brenda <brenda524@naver.com>
2022-03-20 15:04:59 +0900

    #2 - N,M의 의ë¯오해한 의미 해결 / nextInt()로 배열 입력받지 않고, readLine()으로 한 행 문자 입력받은 뒤 charAt()으로 쪼갬

--- src/BruteForce/BOJ_1051.java
+++ src/BruteForce/BOJ_1051.java
@@ -1,41 +1,49 @@
 package BruteForce;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 백준 실버3 "숫자 정사각형"
 // https://www.acmicpc.net/problem/1051
 
+/**
+ * 이 문제는 모든 경우의 수를 다 조사하는 문제이므로, 시간 복잡도를 고려해봐야 한다.
+ * 최대 길이가 50이므로 50 50일 때 size를 50으로 지정하게 된다. 따라서 50*50*50이므로 10억*3을 초과하지 않아 시간이 충분하다.
+ * for문을 돌려서 각 꼭지점 위치의 값이 동일하면, ans에 값을 저장한다.
+ * */
 public class BOJ_1051 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();   //가로
-        int M = sc.nextInt();   //세로
-        int[] arr[] = new int[M][N];
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());    //Q. StringTokenizer st = new StringTokenizer(br.readLine()," ");와 동일한 코드인가요?
+                                                                    //A. Yes. 2번째 인자의 default는 "공백"이다.
+        int N = Integer.parseInt(st.nextToken());   //세로
+        int M = Integer.parseInt(st.nextToken());   //가로
+
+        int[] arr[] = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = input.charAt(i) - '0';    //문자를 숫자로 변환하는 방법 알아두기!
             }
         }
 
         // 꼭짓점에 쓰여 있는 수가 모두 같은 가장 큰 정사각형 찾기
-        int maxSize = 1;    // 0으로 두면 안 됨. 1*1은 가장 작은 정사각형이므로, 최소 정사각형 크기가 1 이상임
-        int a = 2;
-        int b = Math.min(N,M);
-        for (int i=a;i<=b;i++){ //정사각형 한 변의 길이의 범위 => 2<=i<=Math.min(N,M)
-            for(int j=0;j<=M-i;j++){ //세로
-                for(int k=0;k<=N-i;k++){ //가로
-                    if(arr[j][k]==arr[j][k+i-1]){
-                        if(arr[j][k]==arr[j+i-1][k]){
-                            if(arr[j][k]==arr[j+i-1][k+i-1]){
-                                maxSize = i;
-                            }
+        int ans = 1;    // 0으로 두면 안 됨. 1*1은 가장 작은 정사각형이므로, 최소 정사각형 크기가 1 이상임
+        int size = Math.min(N, M);
+        for (int k = 2; k <= size; k++) { //정사각형 한 변의 길이의 범위 => 2<=i<=Math.min(N,M)
+            for (int i = 0; i <= N - k; i++) { //세로
+                for (int j = 0; j <= M - k; j++) { //가로
+                    if (arr[i][j] == arr[i + k - 1][j] && arr[i][j] == arr[i][j + k - 1] && arr[i][j] == arr[i + k - 1][j + k - 1]){
+                        if(ans<k){
+                            ans=k;
                         }
-
                     }
                 }
             }
         }
         // 출력
-        System.out.println(maxSize*maxSize);
+        System.out.println(ans * ans);
     }
 }
