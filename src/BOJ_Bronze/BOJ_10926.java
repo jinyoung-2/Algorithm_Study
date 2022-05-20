3c30b89c46312861b95dfb8384ad6681fec1fa5f
brenda <brenda524@naver.com>
2022-05-20 17:13:11 +0900

    [BOJ] Bronze5 - ??!

---
+++ src/BOJ_Bronze/BOJ_10926.java
@@ -0,0 +1,15 @@
+package BOJ_Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// BOJ_Bronze5 - ??!
+public class BOJ_10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+
+        System.out.println(id+"??!");
+    }
+}
