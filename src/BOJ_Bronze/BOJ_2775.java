77c2062e40d9c8e214cce2002225f75ef139c523
brenda <brenda524@naver.com>
2022-05-30 23:15:03 +0900

    [BOJ] Bronze1 / ë·부녀회장이 될테야

--- src/BOJ_Bronze/BOJ_2775.java
+++ src/BOJ_Bronze/BOJ_2775.java
@@ -1,8 +1,6 @@
 package BOJ_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 // BOJ _ Bronze1 - 부녀회장이 될테야
 // https://www.acmicpc.net/problem/2775
@@ -11,30 +9,25 @@ public class BOJ_2775 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        int maxFloor = 0;
-        int maxRoom = 0;
+        for (int tc = 0; tc < T; tc++) {
+            int k = Integer.parseInt(br.readLine());    //층 ex)1층
+            int n = Integer.parseInt(br.readLine());    //호 ex)3호
 
-        for (int i = 0; i < T; i++) {
-            int k = Integer.parseInt(br.readLine());
-            int n = Integer.parseInt(br.readLine());
-            if (maxFloor < k)
-                maxFloor = k;
-            if (maxRoom < n)
-                maxRoom = n;
-        }
-        int[] result[] =  new int[maxFloor][maxRoom+1];
-        for(int i=1;i<=maxRoom;i++){
-            result[0][i] = i;
-        }
-        for(int i=1;i<=maxFloor;i++){
-            for(int j=1;j<=maxRoom;j++){
-                for(int k=1;i<=)
+            int[] arr[] = new int[k + 1][n + 1];
+            for (int i = 1; i <= n; i++) {
+                arr[0][i] = i;    //0층의 i호에는 i명이 산다
             }
-        }
-        for (int i = 0; i < T; i++) {
-            // 해당 집에 거주민 수를 출력
-            System.out.println();
-        }
 
+            for (int i = 1; i <= k; i++) {
+                for (int j = 1; j <= n; j++) {
+                    for (int m = 1; m <= j; m++) {
+                        arr[i][j] += arr[i - 1][m];
+                    }
+                }
+            }
+
+            // 해당 집에 거주민 수 출력
+            System.out.println(arr[k][n]);
+        }
     }
 }
