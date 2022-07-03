9a18cc17057f3c1c89499ba43f149a5b36be255f
brenda <brenda524@naver.com>
2022-07-03 23:33:53 +0900

    [BOJ] 네 번째 점 / Bronze 3 / 다른 사람 풀이 참고

--- src/Geometry/BOJ_3009.java
+++ src/Geometry/BOJ_3009.java
@@ -1,50 +1,47 @@
 package Geometry;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 // 네 번째 점 / Bronze 3
 // https://www.acmicpc.net/status?user_id=brenda524&problem_id=3009&from_mine=1
 public class BOJ_3009 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int[] cord[] = new int[3][2];
-        int width = 0;  //가로
-        int height = 0; //세로
 
-        for (int i = 0; i < cord.length; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            cord[i][0] = Integer.parseInt(st.nextToken());
-            cord[i][1] = Integer.parseInt(st.nextToken());
+        String[] coord_1 = br.readLine().split(" ");
+        String[] coord_2 = br.readLine().split(" ");
+        String[] coord_3 = br.readLine().split(" ");
+
+        String x, y;
+
+        // x좌표 비교
+        // 1번 x좌표와 2번 x좌표 비교
+        if(coord_1[0].equals(coord_2[0])){
+            x=coord_3[0];
+        }
+        // 1번 x좌표와 3번 x좌표 비교
+        else if(coord_1[0].equals(coord_3[0])){
+            x=coord_2[0];
+        }
+        // 2번 x좌표와 3번 x좌표 비교
+        else {
+            x = coord_1[0];
         }
 
-        // Solution: 쌍을 이루지 않는 각각의 좌표 찾기
-        int ansX = 0;
-        int ansY = 0;
-        int i = 0;
-        if (cord[i][0] == cord[i + 1][0]) {
-            ansX = cord[i + 2][0];
-            if (cord[i][1] == cord[i + 2][1]) {
-                ansY = cord[i + 1][1];
-            } else {   //1,2
-                ansY = cord[i][1];
-            }
-        } else if (cord[i][0] == cord[i + 2][0]) {
-            ansX = cord[i + 1][0];
-            if (cord[i][1] == cord[i + 1][1]) {
-                ansY = cord[i + 2][1];
-            } else {   //1,2
-                ansY = cord[i][1];
-            }
-        } else if (cord[i + 1][0] == cord[i + 2][0]) {
-            ansX = cord[i][0];
-            if (cord[i + 1][1] == cord[i][1]) {
-                ansY = cord[i + 2][1];
-            } else {   //0,2
-                ansY = cord[i + 1][1];
-            }
+        // y 좌표 비교
+        // 1번 x좌표와 2번 x좌표 비교
+        if (coord_1[1].equals(coord_2[1])) {
+            y = coord_3[1];
         }
-        System.out.println(ansX + " " + ansY);
+        // 1번 x좌표와 3번 x좌표 비교
+        else if (coord_1[1].equals(coord_3[1])) {
+            y = coord_2[1];
+        }
+        // 2번 x좌표와 3번 x좌표 비교
+        else {
+            y = coord_1[1];
+        }
+
+        System.out.println(x + " " + y);
     }
 }
